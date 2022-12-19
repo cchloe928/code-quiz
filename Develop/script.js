95104aded5d7942b0d23e51284eed79346288a00
chloegithub <miss_jihyun@hotmail.com>
2022-12-18 20:14:34 -0500

    a

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,9 @@ if (prequestion!=0) {
     document.querySelector (".Q"+prequestion).style="display:none;";
 }
 
+
 // timedCount();
+    document.getElementById ("timer").style="display:block;";
 if (!this.classList.contains("correct")) {
     // time-=10
 console.log("wrong!")
@@ -33,3 +35,65 @@ prequestion ++
 for(i=0; i<start.length; i++) {
     start[i].addEventListener ("click", startQuiz)
 }
+
+
+
+
+
+
+
+
+const Questions = [{
+    id: 0,
+    q: "What is HTML stands for?",
+    a: [{ text: "Hypertext Markup Language", isCorrect: true },
+        { text: "Cascading Style Sheets", isCorrect: false },
+        { text: "Algorithm", isCorrect: false },
+        { text: "jQuery", isCorrect: false }
+    ]
+
+},
+
+{
+    id: 1,
+    q: "What is CSS stands for?",
+    a: [{ text: "Hypertext Markup Language", isCorrect: false },
+        { text: "Cascading Style Sheets", isCorrect: true },
+        { text: "Algorithm", isCorrect: false },
+        { text: "jQuery", isCorrect: false }
+    ]
+
+},
+{
+    id: 2,
+    q: "The condition in an if / else statement is enclosed with _____.",
+    a: [{ text: "quotes", isCorrect: false   },
+        { text: "curly_brackets", isCorrect: false },
+        { text: "parenthesis", isCorrect: true },
+        { text: "Square Brackets", isCorrect: false }
+    ]
+
+},
+
+{
+    id: 3,
+    q: "Arrays in JavaScript can be used to store _____.",
+    a: [{ text: "numbers_and_strings", isCorrect: false },
+        { text: "other_Arrays", isCorrect: false },
+        { text: "booleans", isCorrect: false },
+        { text: "all_of_the_above", isCorrect: true }
+    ]
+
+}
+
+
+.function countdownFunction () {
+    // deduct time so that the clock is counting down in the browser
+    time--;
+    iTimeShow.textContent = time;
+  
+    // create a conditional to see if the user has run out of time
+    if (time <= 0) {
+      thisIsWhereYourQuizEndingFunctionWouldBeCalled();
+    }
+  }
