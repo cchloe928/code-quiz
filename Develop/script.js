3f5643db10a5ddc5d3a13d665af6ae705bec5749
chloegithub <miss_jihyun@hotmail.com>
2022-12-26 16:03:34 -0500

    a

--- Develop/script.js
+++ Develop/script.js
@@ -1,121 +1,118 @@
-const startButton = document.getElementById ("start-btn")
-const nextButton = document.getElementById ("next-btn")
-const questionContainerElement = document.getElementById ("question-container")
-const questionElement= document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-buttons")
-
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
-startButton.addEventListener("click", startGame)
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
 function startGame() {
-    console.log("Started")
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide")
-    setNextQuestion()
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionContainerElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement ("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
 function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
-    }
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
 
-
-function selectAnswer() {
- const selectedButton = e.target
- const correct = selectedButton.dataset.correct
- setStatusClass(document.body, correct)
- Array.from(answerButtonsElement.children).forEach(button => {
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
- })
- if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove("hide")
-    } else {
-        startButton.innerText = "Restart"
-        startButton.classList.remove("hide")
-    }
- }
-
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+    startButton.innerText = 'Clear High Score'
+    startButton.classList.remove('hide')
+  }
+}
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct")
-    } else {
-        element.classList.add("wrong")
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove ("wrong")
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
-
-const question = [
-    {
-        question: "What is HTML stands for?",
+const questions = [
+  {
+    question: "What is HTML stands for?",
         answers: [
-            {text: "Hypertext Markup Language", corect: true },
-            {text: "Cascading Style Sheets", corect: false },
-            {text: "Algorithm", corect: false },
-            {text: "jQuery", corect: false },
-        ]
-    },
-{
+            {text: "Hypertext Markup Language", correct: true },
+            {text: "Cascading Style Sheets", correct: false },
+            {text: "Algorithm", correct: false },
+            {text: "jQuery", correct: false }
+    ]
+  },
+  {
     question: "What is CSS stands for?",
     answers: [
-        {text: "Hypertext Markup Language", corect: false },
-        {text: "Cascading Style Sheets", corect: true },
-        {text: "Algorithm", corect: false },
-        {text: "jQuery", corect: false },
+        {text: "Hypertext Markup Language", correct: false },
+        {text: "Cascading Style Sheets", correct: true },
+        {text: "Algorithm", correct: false },
+        {text: "jQuery", correct: false }
     ]
-},
-{
+  },
+  {
     question: "The condition in an if / else statement is enclosed with _____.",
     answers: [
         {text: "Quotes", corect: false },
-        {text: "Curly Brackets", corect: false },
-        {text: "Parenthesis", corect: true },
-        {text: "Square Backets", corect: false },
+        {text: "Curly Brackets", correct: false },
+        {text: "Parenthesis", correct: true },
+        {text: "Square Backets", correct: false }
     ]
-},
-{
+  },
+  {
     question: "Arrays in JavaScript can be used to store _____.",
     answers: [
-        {text: "Numbers and Strings", corect: false },
-        {text: "Other Arrays", corect: false },
-        {text: "Booleans", corect: false },
-        {text: "All of the above", corect: true },
+        {text: "Numbers and Strings", correct: false },
+        {text: "Other Arrays", correct: false },
+        {text: "Booleans", correct: false },
+        {text: "All of the above", correct: true }
     ]
-}
+  }
 ]
